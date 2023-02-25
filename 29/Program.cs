94d2677b84ce1ebad9bbe96cf72a1e04587b0316
Николай <ibraevnikolay@mail.ru>
2023-02-25 20:01:49 +0500

    Сделали 29 задачу0

---
+++ 29/Program.cs
@@ -0,0 +1,19 @@
+﻿/*Задача 29: Напишите программу, которая задаёт массив из 8 элементов и выводит их на экран.
+
+1, 2, 5, 7, 19 -> [1, 2, 5, 7, 19]
+
+6, 1, 33 -> [6, 1, 33]*/
+
+Console.Clear();
+Console.Write("Введите любое число: ");
+int N = int.Parse(Console.ReadLine());
+int [] array = GetBinArray(N);
+Console.Write($"[{String.Join(", ", array)}]");
+
+int [] GetBinArray(int array){
+    int[] result = new int[array];
+    for (int i = 0; i < N; i++){
+        result[i] = new Random().Next(N+1);
+    }
+    return result;
+}
